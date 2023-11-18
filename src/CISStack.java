25e56a4eee5c26931e810cea865c0d0252967e9f
Adam <adam.ewing.nz@gmail.com>
2023-11-18 13:25:49 +0800

    First commit.

--- src/CISStack.java
+++ src/CISStack.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class CISStack {
+import java.util.Arrays;
+
+public class CISStack {
+
+    // Array property.
+
+    // Size property.
+
+    // Pointer property
+
+    // Constructor.
+
+    // Push. This method pushes a value onto the top of the stack.
+
+    // Pop. This method pops a value from the top of the stack.
+
+    // isEmpty. Returns a boolean indicating whether the linked list is empty.
+
+    // size. Returns the size of the queue.
+
+    // toString. Returns a description of the queue in, for example, the following format:
+    // CISStack{stack=[7, 11], size=2, pointer=1}
+
 }
--- src/CISStackTest.java
+++ src/CISStackTest.java
@@ -1,4 +1,89 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 class CISStackTest {
-  
+
+    private CISStack cisStack;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+
+        cisStack = new CISStack(10);
+
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+
+        cisStack = null;
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void push() {
+
+        assertEquals(0, cisStack.size());
+        cisStack.push(8);
+        assertEquals(1, cisStack.size());
+        cisStack.push(10);
+        assertEquals(2, cisStack.size());
+        cisStack.push(12);
+        assertEquals(3, cisStack.size());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void pop() {
+
+        assertEquals(0, cisStack.size());
+        cisStack.push(8);
+        assertEquals(1, cisStack.size());
+        cisStack.push(10);
+        assertEquals(2, cisStack.size());
+        cisStack.push(12);
+        assertEquals(3, cisStack.size());
+
+        assertEquals(12, cisStack.pop());
+        assertEquals(10, cisStack.pop());
+        assertEquals(8, cisStack.pop());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+
+        assertTrue(cisStack.isEmpty());
+        assertEquals(0, cisStack.size());
+        cisStack.push(8);
+        assertFalse(cisStack.isEmpty());
+        assertEquals(1, cisStack.size());
+        cisStack.push(10);
+        assertFalse(cisStack.isEmpty());
+        assertEquals(2, cisStack.size());
+        cisStack.push(12);
+        assertFalse(cisStack.isEmpty());
+        assertEquals(3, cisStack.size());
+
+        assertEquals(12, cisStack.pop());
+        assertFalse(cisStack.isEmpty());
+        assertEquals(10, cisStack.pop());
+        assertFalse(cisStack.isEmpty());
+        assertEquals(8, cisStack.pop());
+        assertTrue(cisStack.isEmpty());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+
+        String expected = "CISStack{stack=[null, null, null, null, null, null, null, null, null, null], size=0, pointer=-1}";
+        assertEquals(expected, cisStack.toString());
+
+        cisStack.push(8);
+        cisStack.push(10);
+        cisStack.push(12);
+
+        expected = "CISStack{stack=[8, 10, 12, null, null, null, null, null, null, null], size=3, pointer=2}";
+        assertEquals(expected, cisStack.toString());
+
+    }
 }
